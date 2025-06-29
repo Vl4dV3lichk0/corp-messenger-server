7123121a3636b908cdf10839707d24cf3d41c3dd
Vlad <vladvelichko04@mail.ru>
2025-06-29 12:23:14 +0300

    Initial commit.

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from .database import Base
+import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    is_online = Column(Boolean, default=False)
+    last_seen = Column(DateTime, default=datetime.datetime.utcnow)
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=False)
+    sender_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    receiver_id = Column(Integer, ForeignKey("users.id"))  # Для личных сообщений
+    chat_id = Column(Integer, ForeignKey("chats.id"))      # Для групповых
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    is_delivered = Column(Boolean, default=False)
+    is_read = Column(Boolean, default=False)
+
+class Chat(Base):
+    __tablename__ = "chats"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    is_group = Column(Boolean, default=False)
+    # Связи будут реализованы через ассоциативную таблицу
